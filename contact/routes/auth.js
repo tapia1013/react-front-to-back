a07e29d27beb224f510f914d906dc2b2ff96cc74
tapia1013 <manbearpig1013@gmail.com>
2021-05-14 16:48:46 -0700

    auth expire add

--- contact/routes/auth.js
+++ contact/routes/auth.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+
+
+
 
 // @route       GET api/auth
 // @desc        Get logged in user
@@ -8,12 +17,69 @@ router.get('/', (req, res) => {
   res.send('Get logged in user');
 })
 
+
+
+
 // @route       POST api/auth
 // @desc        Auth user and get token
 // @access      Public
-router.post('/', (req, res) => {
-  res.send('Log in user');
-})
+router.post(
+  '/',
+  [
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email })
+
+      if (!user) {
+        return res.status(400).json({ msg: 'Invlaid Credentials' })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' })
+      }
+
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      )
+
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error')
+    }
+
+  })
+
+
+// video 5 auth middleware
 
 
 module.exports = router;
--- contact/routes/users.js
+++ contact/routes/users.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config')
 const { check, validationResult } = require('express-validator/check')
 
 
@@ -19,15 +22,68 @@ router.post('/', [
   check('password', 'Please enter a password with 6 or more characters').isLength({
     min: 6
   })
-], (req, res) => {
-  const errors = validationResult(req)
-  // check to see if err is empty
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-  }
+],
+  async (req, res) => {
+    const errors = validationResult(req)
+    // check to see if err is empty
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
 
-  res.send('passed')
-})
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email })
+
+      if (user) {
+        return res.status(400).json({ msg: 'User alreadt exists' })
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      // encrypt
+      const salt = await bcrypt.genSalt(10);
+      // hash pw
+      user.password = await bcrypt.hash(password, salt)
+
+      // save hash/salted pw/user to DB
+      await user.save();
+
+
+      // send back JWT
+      // res.send('User saved to DB')
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      // signToken to generate token (payload, secret, {options})
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      )
+
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+    }
+
+
+
+  })
 
 
 module.exports = router;
