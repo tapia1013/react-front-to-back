fab9329d863722cfe9a921b17015074cc950f17c
tapia1013 <manbearpig1013@gmail.com>
2021-05-31 14:48:52 -0700

    added private route logout and if is signed wiith private route..... issues it not rendering

---
+++ contact/client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import AuthContext from '../../context/auth/authContext';
+import { Route, Redirect } from 'react-router-dom';
+
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, loading } = authContext;
+
+
+  // creating private route component 
+  return (
+    <Route {...rest} render={props => !isAuthenticated && !loading ? (
+      <Redirect to='/login' />
+    ) : (
+      <Component {...props} />
+    )} />
+  )
+}
+
+
+
+
+
+export default PrivateRoute;
+
+
+
+
+
+
